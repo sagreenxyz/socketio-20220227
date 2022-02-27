38915fcdf871fdd277b0f2d00ea8709b7d35e978
Scott Green <sagreenxyz@gmail.com>
2022-02-27 13:43:10 -0600

    Installing socket.io and setup index.html

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express(); // function handler to supply to an HTTP server
 const http = require('http');
 const server = http.createServer(app); // supplying that function handler to the HTTP server
+const {Server} = require('socket.io');
+const io = new Server(server); // initialized by passing in server object
 
 app.get('/', (req, res) => { // route handler for home page
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
 server.listen(3001, () => {
